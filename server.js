e9a5b25842823d04de3acfeec2b9cf735a65b5cb
Eugene Grinberg <eugrin@gmail.com>
2017-01-12 17:45:24 -0500

    Add disconnect event and message

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ var moment = require('moment');
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
-
+//'connection' and 'disconnect' are built in names. joinRoom and message are not.
 io.on('connection', function(socket) {
   console.log('User connected via socket.io!');
+
+  socket.on('disconnect', function(){
+    var userData = clientInfo[socket.id];
+    if(typeof userData !== 'undefined') {
+      socket.leave(userData.room);
+      io.to(userData.room).emit('message',  {
+        name: 'System',
+        text: userData.name + ' has left!',
+        timestamp: moment().valueOf()
+      });
+      delete clientInfo[socket.id];
+    }
+  });
 
   socket.on('joinRoom', function(req){
     clientInfo[socket.id] = req;
