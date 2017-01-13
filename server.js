74bc1c8cf250705a11c7a8f9f4f861bc111fb4b9
Eugene Grinberg <eugrin@gmail.com>
2017-01-12 20:20:39 -0500

    Add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ var moment = require('moment');
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
+
+//Sends currentUsers to provided socket
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
+
+  if(typeof info === 'undefined') {
+    return;
+  }
+
+  Object.keys(clientInfo).forEach(function(socketId){
+    var userInfo = clientInfo[socketId];
+
+    if(info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current users: ' + users.join(', '),
+    timestamp: moment().valueOf()
+  });
+}
 //'connection' and 'disconnect' are built in names. joinRoom and message are not.
 io.on('connection', function(socket) {
   console.log('User connected via socket.io!');
@@ -39,9 +63,12 @@ io.on('connection', function(socket) {
   socket.on('message', function(message){
     console.log('Message received: ' + message.text);
     //console.log(JSON.stringify(message));
-    message.timestamp = moment().valueOf();
-
-    io.to(clientInfo[socket.id].room).emit('message', message);
+    if(message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
   });
 //Send this message to the client when it's connected.
   socket.emit('message', {
