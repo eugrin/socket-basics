760b4ff826ef651ad718d12e7b5e98254b026726
Eugene Grinberg <eugrin@gmail.com>
2017-01-14 22:00:00 -0500

    Add bottstrap and styles

--- moment-examples.js
+++ moment-examples.js
@@ -6,9 +6,9 @@ var now = moment();
 // now.subtract(1, 'year');
 // console.log(now.format('X'));
 //console.log(now.format('x'));
-console.log(now.valueOf());
+console.log(now.format('hh:mm.SSS'));
 
 var timestamp = now.valueOf();
 var timestampMoment = moment.utc(timestamp);
-console.log(timestampMoment.local().format('h:mm a'));
+console.log(timestampMoment.local().format('hh:mm.SSS'));
 // console.log(now.format('MMM Do YYYY, h:mm a'));
